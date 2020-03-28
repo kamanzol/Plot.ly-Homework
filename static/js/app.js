b8a966a8a1cb64c5f5082178d8b35297cb4e18b2
kamanzol <katie.manzoline@gmail.com>
2020-03-28 18:57:31 -0500

    break time

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,37 @@
 function DrawBargraph(sampleId)
 {
     console.log(`Calling DrawBargraph(${sampleId})`);
+    d3.json("samples.json").then((data) => {
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+
+        }
+
+        barArray = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+        };
+
+        Plotly.newPlot("bar", barArray, barLayout);
+
+    });
+
 }
 
 function DrawBubblechart(sampleId)
@@ -11,6 +42,27 @@ function DrawBubblechart(sampleId)
 function ShowMetadata(sampleId)
 {
     console.log(`Calling ShowMetadata(${sampleId})`);
+
+    d3.json("samples.json").then((data) => {
+
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(md => md.id == sampleId);
+        var result = resultArray[0];
+
+        var Panel = d3.select("#sample-metadata");
+
+        Panel.html("");
+        Object.entries(result).forEach(([key, value]) => {
+            var textToShow = "Lots of stuff here!"; // add keys and values with $
+            Panel.append("h6").text(textToShow);
+
+        });
+    });
+}
+
+function DrawGauge(sampleId)
+{
+    console.log(`Calling DrawGauge(${sampleId})`);
 }
 
 
@@ -20,6 +72,7 @@ function optionChanged(newSampleId)
 
     DrawBubblechart(newSampleId);
     DrawBargraph(newSampleId);
+    DrawGauge(newSampleId);
     ShowMetadata(newSampleId);
 
 }
@@ -40,7 +93,15 @@ function InitDashboard()
             .text(sampleId)
             .property("value", sampleId);
 
-        })
+        });
+
+        var sampleId = sampleNames[0];
+
+        DrawBargraph(sampleId);
+        DrawBubblechart(sampleId);
+        DrawGauge(sampleId);
+        ShowMetadata(sampleId);
+
     });
 }
 
